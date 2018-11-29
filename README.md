f89227e41bfd86fb78f193c9b19658bc58d66070
Menny Even-Danan <menny@spotify.com>
2018-11-29 11:48:47 -0500

    Empty Android app
    Plus, Feather icons set

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# navajo-chat
+# Navajo
+![Logo](assets/web_hi_res_512.png "Najajo")
+
 Instant Messaging
---
+++ clients/android/app/src/main/java/net/evendanan/navajo/MainActivity.java
@@ -0,0 +1,6 @@
+package net.evendanan.navajo;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
